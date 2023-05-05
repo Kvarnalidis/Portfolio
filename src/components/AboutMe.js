44ac51cc20edca01b5b24fadfd39273cb6966e95
Kvarnalidis <kostasvarnalidis@gmail.com>
2023-05-05 11:11:01 +0300

    centered aboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,14 +16,12 @@ const AboutMe=forwardRef(({Contact},AboutMe) =>{
             </h2>
             <p>
               I am an Full-Stack developer with a passion to learn.My biggest advantage is my ability to teach my self new skills.
-              I love coding and basketball and right now i am try to develop a stats app for the  Euroleague and the NBA.
+              I love coding and basketball and right now I am try to develop a stats app for the  Euroleague and the NBA.
             </p>
             <div className='buttons'>
             <a href={resume} download="Resume.pdf" target="_blank" rel="noreferrer"><button className="button">Resume</button></a>
             <button className="button" onClick={()=>Contact.current.scrollIntoView({behavior:"smooth"})} ><span>Contact Me </span></button>
             </div>
-        </div>
-        <div className='ilu'>
         </div>
     </div>
   )
